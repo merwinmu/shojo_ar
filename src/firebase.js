7a50e91acdcf0d66db84f316c12ea0c9ca7307c5
merwinmu <43412175+merwinmu@users.noreply.github.com>
2021-04-10 03:10:44 +0200

    Changed 3D model

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,13 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- 
+    apiKey: "AIzaSyDN63n4fPbwMBaebxT09pW_UZDKKajwQWw",
+    authDomain: "shojo-ar.firebaseapp.com",
+    projectId: "shojo-ar",
+    storageBucket: "shojo-ar.appspot.com",
+    messagingSenderId: "548554521021",
+    appId: "1:548554521021:web:140f35d817af3c8d743ad4",
+    measurementId: "G-5VJPVBBPJ8"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
